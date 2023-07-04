e504ae20acad9a6bb28c8714994cdd3c37ddf14b
Tanisha Pattnaik <tanishakhuc@gmail.com>
2023-07-04 20:20:24 +0530

    level addition

--- game.js
+++ game.js
@@ -1,8 +1,36 @@
-var gamePattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level = 0;
+
+var started = false;
+
+$(document).on("keypress", function(){
+    if(!started){
+        $("h1").text("Level "+ level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click",function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    // var audio = new Audio("sounds/"+ userChosenColour + ".mp3");
+    // audio.play();
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
 function nextSequence() {
+
+    level++;
+    $("h1").text("Level "+ level);
+
     var randomNumber = Math.floor(Math.random()*4);
 
     var randomChosenColour = buttonColours[randomNumber];
@@ -11,12 +39,23 @@ function nextSequence() {
 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("/sounds/"+ randomChosenColour + ".mp3");
-    audio.play();
-
-    $("button").click(function() {
-        var userChosenColour = $("button")
-    });
+    playSound(randomChosenColour);
+    // var audio = new Audio("sounds/"+ randomChosenColour + ".mp3");
+    // audio.play();
+    
+    
 };
 
-nextSequence();
+function playSound(name) {
+    var audio = new Audio("sounds/"+ name + ".mp3");
+    audio.play();
+};
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+};
+
